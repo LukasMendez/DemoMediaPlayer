bb3b7d40e460d4b3bbcb260b722880af5c6b9906
LukasMendez <43059840+LukasMendez@users.noreply.github.com>
2019-01-07 13:40:40 +0100

    Made changes to the buttons and created a dark pane on the left side

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.image.Image;
 import javafx.scene.media.*;
 import javafx.scene.control.Button;
 
@@ -8,16 +9,19 @@ import javafx.fxml.Initializable;
 import java.io.*;
 import java.net.*;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class Controller implements Initializable {
     @FXML
     private MediaView mediaV;
 
     @FXML
-    private Button play;
+    private Button playPauseButton;
 
     private MediaPlayer mp;
     private Media me;
+
+    private boolean isPlaying = false; // Will check if the music player is playing
 
 
     /**
@@ -43,12 +47,92 @@ public class Controller implements Initializable {
 
     @FXML
     /**
-     * Handler for the play button
+     * Handler for the play and pause button.
      */
     private void handlePlay()
     {
-        // Play the mediaPlayer with the attached media
+
+        if (!isPlaying){
+
+
+            // Play the mediaPlayer with the attached media
+            mp.play();
+
+            String pathPause = "/sample/buttons/pause.png"; // The path has to be relative in order for CSS to find the image and treat it.
+
+            playPauseButton.setStyle("-fx-background-size: 64px 64px; " + "-fx-background-color:  #1C1C1C;" + "-fx-background-image: url('" + pathPause + "'); ");
+
+            System.out.println("User pressed the play button");
+
+            isPlaying=true;
+
+
+        } else {
+
+            // If the isPlaying boolean is set to true, it will skip the if statement and pause the music
+
+            mp.pause();
+
+            String pathPlay = "/sample/buttons/play.png"; // The path has to be relative in order for CSS to find the image and treat it.
+
+            playPauseButton.setStyle("-fx-background-size: 64px 64px; " + "-fx-background-color:  #1C1C1C;" + "-fx-background-image: url('" + pathPlay + "'); ");
+
+
+            System.out.println("User pressed the pause button");
+
+            isPlaying=false;
+
+        }
+
+
+
+    }
+
+    /**
+     * Will stop the music completely
+     */
+
+    @FXML
+    private void handleStop(){
+
+        mp.stop();
+
+        String pathPlay = "/sample/buttons/play.png"; // The path has to be relative in order for CSS to find the image and treat it.
+
+        playPauseButton.setStyle("-fx-background-size: 64px 64px; " + "-fx-background-color:  #1C1C1C;" + "-fx-background-image: url('" + pathPlay + "'); ");
+
+
+        isPlaying=false;
+
+        Scanner in = new Scanner(System.in);
+
+       // System.out.println("Enter song name: ");
+
+       // String nameOfSong = in.nextLine();
+
+       // changeSong(nameOfSong);
+
+
+        // TESTING
+
+    }
+
+
+    private void changeSong(String filename){
+
+        // Build the path to the location of the media file
+        String path = new File("src/sample/media/"+filename).getAbsolutePath();
+        // Create new Media object (the actual media content)
+        me = new Media(new File(path).toURI().toString());
+        // Create new MediaPlayer and attach the media to be played
+        mp = new MediaPlayer(me);
+
+        // Will play the selected song
         mp.play();
+
+
+
+
     }
 
 
